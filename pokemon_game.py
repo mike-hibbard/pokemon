e8e52dac63cfa9782e26eb1d9b3788c62916fab5
mikejhibbard <mikejhibbard@gmail.com>
2024-11-16 16:51:48 +0000

    Added data, basic extraction

--- pokemon_game.py
+++ pokemon_game.py
@@ -2,6 +2,44 @@
  A command line game for catching pokemon
 """
 
+from pathlib import Path
+import csv
+
+
+# Read and extract Pokemon database
+path = Path('data/pokemon.csv')
+
+# Parse into a list of lines
+lines = path.read_text(encoding='utf-8').splitlines()
+
+# Give the lines list to a reader object
+reader = csv.reader(lines)
+
+# Use the next method to (initially) get headers, and assign to a variable.
+# By assigning to the variable we can retain the headers...
+# - calling next again will yield the first line of data, and so on...
+# NOTE: next() is a function NOT a method of reader!
+header_row = next(reader)
+
+# prints ['ID','Name','Form','Type1','Type2','Total','HP','Attack','Defense','Sp. Atk','Sp. Def','Speed','Generation']
+# print(header_row)
+
+# helps us understand the headers
+# for index, column_header in enumerate(header_row):
+#   print(index, column_header)
+
+# Extract data from columns by index - just as you would from a list.
+# NOTE: reader tracks position in the file - the below works because we extracted
+# the headers first, and the start position for row is the first data row, not
+# the header row.
+
+# Extract pokemon name, type, total
+pokemon_details = []
+for row in reader:
+    pokemon = (row[1],row[3],row[5])
+    pokemon_details.append(pokemon)
+
+print(pokemon_details)
 
 """
  Rules and Game Logic
